9da4cd1673bbc7b79a8533924f8c8cceeb0ba898
Juan Menichetti <jpmenichetti13@gmail.com>
2022-05-18 10:51:05 -0300

    se agrego el ejercicio 1

---
+++ js/ejercicio1-tp2.js
@@ -0,0 +1,7 @@
+let edad = parseInt(prompt("¿Cual es su edad?"));
+
+if(edad >= 18){
+    document.write("Usted esta habilitado para conducir.")
+}else{
+    document.write("Usted es menor de edad.")
+}
