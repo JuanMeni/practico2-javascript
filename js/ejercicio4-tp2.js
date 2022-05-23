9de9145aa7ba963883743eb6a3af4193e34df7db
Juan Menichetti <jpmenichetti13@gmail.com>
2022-05-23 10:21:55 -0300

    se agrego el ejercicio 4

---
+++ js/ejercicio4-tp2.js
@@ -0,0 +1,15 @@
+// Realiza un script que pida números hasta que se pulse “cancelar”. Si no es un número deberá indicarse con un «alert» y seguir pidiendo números. Al salir con “cancelar” deberá indicarse la suma total de los números introducidos.
+
+let suma = 0;
+
+do {
+  var numero = prompt("Introduzca un numero");
+
+  if (Number(numero) == numero) {
+    numero = Number(numero);
+    suma = suma + numero;
+  } else if (numero != undefined) {
+      alert(numero + " No es un numero");
+  }
+} while (numero != undefined);
+document.write(suma);
