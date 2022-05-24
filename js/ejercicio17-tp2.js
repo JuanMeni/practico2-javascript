71d9900bd85046eb7a648373aec12b6f0018fd1c
Juan Menichetti <jpmenichetti13@gmail.com>
2022-05-24 09:41:24 -0300

    se agrego el ejercicio 17

--- js/ejercicio17-tp2.js
+++ js/ejercicio17-tp2.js
@@ -1,2 +1,13 @@
 // Realiza un script que muestre la posición de la primera vocal de un texto introducido por teclado.
 
+let texto= prompt("Introduzca un texto");
+let i = 0;
+while(i < texto.length){
+ 
+ if (texto.charAt(i)=='a' || texto.charAt(i)=='e'|| texto.charAt(i)=='i' || texto.charAt(i)=='o' || texto.charAt(i)=='u')
+ {
+    document.write("El texto tiene su vocal en la posicion "+ (i+1));
+    i = texto.length;
+ }
+ i++;
+}  
