31f7fab761c45b673a0562744b142a9f192e6c9f
Juan Menichetti <jpmenichetti13@gmail.com>
2022-05-24 09:17:39 -0300

    corrigiendo los ejercicios

--- js/ejercicio2-tp2.js
+++ js/ejercicio2-tp2.js
@@ -11,33 +11,6 @@
 
 let nota = prompt("Ingrese su nota: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10");
 
-// switch (nota) {
-//   case "0":
-//   case "1":
-//   case "2":
-//     alert("Su nota es muy insuficiente.");
-//     break;
-//   case "3":
-//   case "4":
-//     alert("Su nota es insuficiente.");
-//     break;
-//   case "5":
-//   case "6":
-//     alert("Su nota es sufciente.");
-//   case "7":
-//     alert("Su nota es un bien.");
-//     break;
-//   case "8":
-//   case "9":
-//     alert("Su nota es notable.");
-//     break;
-//   case "10":
-//     alert("Su nota es sobresaliente.");
-//     break;
-//   default:
-//     document.write("Ingreso un numero erroneo.");
-// }
-
 if(Number(nota) == nota){
     switch (nota) {
         case "0":
