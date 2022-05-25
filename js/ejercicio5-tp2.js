de1752d6eb83bd94ceb4af150dda37f9e4c52db0
Juan Menichetti <jpmenichetti13@gmail.com>
2022-05-25 16:56:16 -0300

    se agrego el ejercicio 5

--- js/ejercicio5-tp2.js
+++ js/ejercicio5-tp2.js
@@ -9,4 +9,88 @@ Si lo introducido no es un número deberá indicarse con un alert y volver a pre
 Deberá de repetirse el proceso hasta que el usuario pulse «cancelar».
 */
 
-let dni = prompt("Ingrese su DNI.");
+let resta = 0;
+let letra = "";
+
+do{
+    let dni = parseInt(prompt("Ingrese su DNI."));
+    if(dni >= 0 && dni <= 99999999){
+        resta = dni % 23;
+        switch (resta){
+            case 0:
+                letra = "T";
+                break;
+            case 1:
+                letra = "R";
+                break;
+            case 2:
+                letra = "W";
+                break;
+            case 3:
+                letra = "A";
+                break;
+            case 4:
+                letra = "G";
+                break;
+            case 5:
+                letra = "M";
+                break;
+            case 6:
+                letra = "Y";
+                break;
+            case 7:
+                letra = "F";
+                break;
+            case 8:
+                letra = "P";
+                break;
+            case 9:
+                letra = "D";
+                break;
+            case 10:
+                letra = "X";
+                break;
+            case 11:
+                letra = "B";
+                break;
+            case 12:
+                letra = "N";
+                break;
+            case 13:
+                letra = "J";
+                break;
+            case 14:
+                letra = "Z";
+                break;
+            case 15:
+                letra = "S";
+                break;
+            case 16:
+                letra = "Q";
+                break;
+            case 17:
+                letra = "V";
+                break;
+            case 18:
+                letra = "H";
+                break;
+            case 19:
+                letra = "L";
+                break;
+            case 20:
+                letra = "C";
+                break;
+            case 21:
+                letra = "K";
+                break;
+            case 22:
+                letra = "E";
+                break;
+            default:
+                alert("Numero incorrecto.")
+        }
+        alert("El DNI "+ dni +" le corresponde la letra "+ letra);
+    } else if(dni != undefined){
+        alert(dni + " no es un numero.");
+    }
+}while(confirm("Desea ingresar otro numero de DNI?") == true);
